07bcad4d858c5427629c2b6ad9d4f9ce54538ba5
MSI <MSI@DESKTOP-P8U90B1>
2021-02-21 17:14:25 +0900

    controller if update

--- src/pjt/one/com/controller/BoardController.java
+++ src/pjt/one/com/controller/BoardController.java
@@ -1,11 +1,17 @@
 package pjt.one.com.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import pjt.one.com.dao.BoardDao;
+import pjt.one.com.vo.BoardListVo;
+
 
 @WebServlet("/board")
 public class BoardController extends HttpServlet {
@@ -21,6 +27,9 @@ public class BoardController extends HttpServlet {
 		
 		String cmd = request.getParameter("cmd");
 		if(cmd.equals("FISRTLIST")) {
+			BoardDao dao= new BoardDao();
+			List<BoardListVo> list = dao.getBoardList();
+			
 			String link = "/view/list.jsp";
 			request.getRequestDispatcher(link).forward(request, response);
 		}
